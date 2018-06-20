19a4f2d25da66405aa947f2562b90ce5e7faecef
ToddMarshall-QW <qwadzie@gmail.com>
2018-06-20 14:46:40 -0400

    new prod version 20180620

--- services/passport.js
+++ services/passport.js
@@ -6,35 +6,34 @@ const keys = require('../config/keys');
 const User = mongoose.model('users');
 
 passport.serializeUser((user, done) => {
+  console.log("serializeUser:"+user.id)
   done(null, user.id);
 });
 
 passport.deserializeUser((id, done) => {
   User.findById(id).then(user => {
+    console.log("deserializeUser"+id+"...user:"+JSON.stringify(user))
     done(null, user);
   });
 });
-
-passport.use(
-  new GoogleStrategy(
-    {
-      clientID: keys.googleClientID,
+passport.use(new GoogleStrategy(    {
+      clientID:     keys.googleClientID,
       clientSecret: keys.googleClientSecret,
       callbackURL: '/auth/google/callback',
-      proxy: true
+      //proxy: true
     },
-    (accessToken, refreshToken, profile, done) => {
-      User.findOne({ googleId: profile.id }).then(existingUser => {
-        if (existingUser) {
-          // we already have a record with the given profile ID
-          done(null, existingUser);
-        } else {
-          // we don't have a user record with this ID, make a new record!
-          new User({ googleId: profile.id })
-            .save()
-            .then(user => done(null, user));
-        }
-      });
-    }
-  )
-);
+    (accessToken,refreshToken,profile,done) =>
+     {
+       User.findOne({ googleId: profile.id }).then(existingUser => {
+         if (existingUser) {
+           // we already have a record with the given profile ID
+           console.log("Existing User:",existingUser.googleId);
+           done(null, existingUser);
+         } else {
+           // we don't have a user record with this ID, make a new record!
+           new User({ googleId: profile.id })
+             .save()
+             .then(user => {console.log("New User Saved:",user.googleId); done(null, user) });
+         }
+       })}
+ ));
