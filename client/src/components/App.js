5a07fffb1503fe4cc99fd54cd5e06492183ddad8
ToddMarshall-QW <qwadzie@gmail.com>
2018-06-27 10:17:39 -0400

    added client subfolders for client86needfix, fix auth in returning to app's browser tab

---
+++ client/src/components/App.js
@@ -0,0 +1,45 @@
+import React, {Component} from 'react';
+import {BrowserRouter,Route} from 'react-router-dom';
+import {connect} from 'react-redux'
+import * as actions from '../actions'
+
+import Header from './Header';
+import Landing from './Landing'
+
+const Dashboard = ()=><h2>Dashboard</h2>
+const SurveyNew = ()=><h2>SurveyNew</h2>
+
+class App extends Component {
+  componentWillUnmount() {
+    alert("will unmount")
+    window.removeEventListener('resize',this.handleResize);
+    window.removeEventListener('focus',this.handleFocus);
+  }
+  componentDidMount() {
+    //alert("did mount")
+    window.addEventListener('resize',this.handleResize);
+    window.addEventListener('focus',this.handleFocus);
+    this.props.fetchUser();
+  }
+  handleResize() {
+    alert("resize event")
+  }
+  handleFocus() {
+    alert("Focus event")
+  }
+  render() {
+    return (
+    <div className='container'>
+    <BrowserRouter>
+    <div>
+    <Header />
+    <Route path="/" exact component={Landing} />
+    <Route path="/surveys" exact component={Dashboard} />
+    <Route path="/surveys/new" component={SurveyNew} />
+    </div>
+    </BrowserRouter>
+    </div>
+    );
+}
+  }
+export default connect(null,actions)(App);
