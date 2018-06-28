371a1509c165b6b9f52cd1d99fb92c4a9f14a641
ToddMarshall-QW <qwadzie@gmail.com>
2018-06-27 20:35:24 -0400

    App.js uses body and auth test in render

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -4,42 +4,35 @@ import {connect} from 'react-redux'
 import * as actions from '../actions'
 
 import Header from './Header';
-import Landing from './Landing'
+import Body from './Body'
 
 const Dashboard = ()=><h2>Dashboard</h2>
-const SurveyNew = ()=><h2>SurveyNew</h2>
+const SurveyNew = ()=><Header /> //<div><div><Header /></div><h2>SurveyNew</h2></div>
 
 class App extends Component {
   componentWillUnmount() {
     alert("will unmount")
-    window.removeEventListener('resize',this.handleResize);
-    window.removeEventListener('focus',this.handleFocus);
+    window.removeEventListener('focus',this.handleFocus.bind(this));
   }
   componentDidMount() {
-    //alert("did mount")
-    window.addEventListener('resize',this.handleResize);
-    window.addEventListener('focus',this.handleFocus);
     this.props.fetchUser();
-  }
-  handleResize() {
-    alert("resize event")
+    window.addEventListener('focus',this.handleFocus.bind(this));
   }
   handleFocus() {
-    console.log("<><><><><><>>>--->Focus event")
+    console.log("--->Focus")
+    if (this.props && this.props.fetchUser)
+      this.props.fetchUser();
   }
   render() {
-    return (
-    <div className='container'>
-    <BrowserRouter>
-    <div>
-    <Header />
-    <Route path="/" exact component={Landing} />
-    <Route path="/surveys" exact component={Dashboard} />
-    <Route path="/surveys/new" component={SurveyNew} />
-    </div>
-    </BrowserRouter>
-    </div>
-    );
+      switch (this.props.auth) {
+      case false:
+      case null:
+        return (<div className='containter'><BrowserRouter><div><Header /><Body /></div></BrowserRouter></div>);
+      default:
+        return (<div className='containter'><BrowserRouter><div><Header /><Body /></div></BrowserRouter></div>);
+      }
 }
-  }
+}
+
+
 export default connect(null,actions)(App);
